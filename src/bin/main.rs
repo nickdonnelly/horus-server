aff6d1677c82677cfd797a0f468a9579008f5940
Nicholas Donnelly <nick@donnelly.cc>
2017-09-10 14:34:13 +0200

    Added directory creation for the storage of images.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,40 +4,14 @@
 extern crate horus_server;
 extern crate rocket;
 extern crate rocket_contrib;
-#[macro_use] extern crate diesel;
-#[macro_use] extern crate diesel_codegen;
 
 use horus_server::*;
-use self::models::*;
-use self::DbConn;
-use diesel::prelude::*;
 use rocket_contrib::Template;
 
-use std::collections::HashMap;
-
-#[get("/<name>")]
-fn index(name: String, conn: DbConn) -> Template {
-    use horus_server::schema::horus_users::dsl::*;
-
-    let mut context = HashMap::new();
-    //context.insert("name", name);
-
-    let results = horus_users.filter(first_name.eq(name))
-        .load::<User>(&*conn)
-        .expect("error");
-    
-    for user in results {
-        context.insert("name", user.email);
-        //println!("email {}", user.email);
-    }
-
-    Template::render("index", &context)
-}
-
 fn main() {
+    check_dirs();
     rocket::ignite()
         .attach(Template::fairing())
-        .mount("/name/", routes![index])
         .mount("/user", routes![self::routes::user::show])
         .mount("/user", routes![self::routes::user::update])
         .mount("/license", routes![self::routes::key::validity_check])
@@ -47,4 +21,23 @@ fn main() {
         .mount("/paste", routes![self::routes::paste::delete])
         .manage(self::dbtools::init_pool())
         .launch();
+}
+
+fn check_dirs() {
+    use std::fs;
+    use std::path::Path;
+
+    let mut directories: Vec<String> = Vec::new();
+    directories.push("live/images/".to_string());
+    directories.push("live/images/thumbnails".to_string());
+    directories.push("live/videos/".to_string());
+    directories.push("live/videos/thumbnails".to_string());
+    directories.push("live/files/".to_string());
+    
+    for dirstr in directories {
+        let path: &Path = Path::new(&dirstr);
+        if !path.exists() {
+            fs::create_dir_all(path).unwrap();
+        }
+    }
 }
