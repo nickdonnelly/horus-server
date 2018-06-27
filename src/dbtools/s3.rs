1dcea9143d545af619c87f03cc062ea5485bec9a
Nicholas Donnelly <nick@donnelly.cc>
2018-06-27 10:04:27 +0200

    Move bucket creation code to separate method

--- src/dbtools/s3.rs
+++ src/dbtools/s3.rs
@@ -14,11 +14,12 @@ fn get_s3_creds() -> Credentials
     Credentials::new(&::AWS_ACCESS, &::AWS_SECRET, None)
 }
 
+/// Delete the object in the s3 bucket at the given path
+/// Returns a string containing the data given by S3, or a unit
+/// if an error occured
 pub fn delete_s3_object(path: &str) -> Result<String, ()>
 {
-    let creds = get_s3_creds();
-    let region = REGION.parse::<self::s3::region::Region>().unwrap();
-    let bucket = Bucket::new(BUCKET, region, creds);
+    let bucket = get_bucket();
     let res = bucket.delete(path);
     if res.is_err() {
         return Err(());
@@ -33,9 +34,7 @@ pub fn delete_s3_object(path: &str) -> Result<String, ()>
 /// returns: a public URL to the object
 pub fn resource_to_s3_named(filename: &str, path: &str, data: &Vec<u8>) -> Result<String, ()>
 {
-    let creds = get_s3_creds();
-    let region = REGION.parse::<self::s3::region::Region>().unwrap();
-    let mut bucket = Bucket::new(BUCKET, region, creds);
+    let mut bucket = get_bucket() ;
     let mut dispositionstr = String::from("attachment; filename=\"");
     dispositionstr += filename;
     dispositionstr += "\"";
@@ -50,7 +49,7 @@ pub fn resource_to_s3_named(filename: &str, path: &str, data: &Vec<u8>) -> Resul
     Ok(String::from_utf8(by).unwrap())
 }
 
-/// Upload to S3 using the private canned ACL.
+/// Upload to S3 using the private canned ACL. Prevents access without a presigned URL.
 /// returns: the path to the s3 object from the root of the bucket (not a url)
 pub fn private_resource_to_s3_named(
     filename: &str,
@@ -58,13 +57,12 @@ pub fn private_resource_to_s3_named(
     data: &Vec<u8>,
 ) -> Result<String, ()>
 {
-    let creds = get_s3_creds();
-    let region = REGION.parse::<self::s3::region::Region>().unwrap();
-    let mut bucket = Bucket::new(BUCKET, region, creds);
+    let mut bucket = get_bucket();
     let mut dispositionstr = String::from("attachment; filename=\"");
     dispositionstr += filename;
     dispositionstr += "\"";
-    bucket.add_header("x-amz-acl", "private"); // only we can read
+    // Don't allow it to be read:
+    bucket.add_header("x-amz-acl", "private");
     bucket.add_header("content-disposition", &dispositionstr);
     let (_, code) = bucket
         .put(&path, &data, "application/octet-stream")
@@ -77,19 +75,23 @@ pub fn private_resource_to_s3_named(
     }
 }
 
+/// Send the given byte vector s3 on the given path with public visibility.
+/// Returns the string that s3 responds with or a unit on error.
 pub fn resource_to_s3(path: &str, data: &Vec<u8>) -> Result<String, ()>
 {
-    let creds = get_s3_creds();
-    let region = REGION.parse::<self::s3::region::Region>().unwrap();
-    let mut bucket = Bucket::new(BUCKET, region, creds);
-    bucket.add_header("x-amz-acl", "public-read"); // this way we can serve it later
+    let mut bucket = get_bucket();
+    // Anybody can view it:
+    bucket.add_header("x-amz-acl", "public-read");
+    // Set the disposition so it knows where to look
     bucket.add_header("content-disposition", "attachment");
 
+    // Send it as text data
     let (by, code) = bucket.put(&path, &data, "text/plain").unwrap();
 
     if code != 200 {
         return Err(());
     }
+
     Ok(String::from_utf8(by).unwrap())
 }
 
@@ -98,9 +100,11 @@ pub fn get_s3_presigned_url(path: String) -> Result<String, String>
 {
     use self::process::Command;
 
+    // The string we pass into the cli to get the url
     let mut url_base = "s3://".to_string() + BUCKET;
     url_base += path.as_str();
 
+    // Use the AWS CLI, as building the string manually is quite involved.
     let url = Command::new("aws")
         .arg("s3")
         .arg("presign")
@@ -119,4 +123,10 @@ pub fn get_s3_presigned_url(path: String) -> Result<String, String>
         let url = url.unwrap();
         Ok(String::from_utf8_lossy(&url.stdout).to_string())
     }
+}
+
+/// Returns the bucket that can be used for accessing s3.
+fn get_bucket() -> Bucket
+{
+    Bucket::new(BUCKET, REGION.parse::<self::s3::region::Region>().unwrap(), get_s3_creds())
 }
